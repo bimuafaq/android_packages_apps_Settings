98b84bedf2e3eae783162f10a7ca65e751109122
Dmitry Muhomor <muhomor.dmitry@gmail.com>
2022-04-17 18:00:53 +0300

    add an ability to disable non-system apps from the "App info" screen

--- src/com/android/settings/applications/appinfo/AppButtonsPreferenceController.java
+++ src/com/android/settings/applications/appinfo/AppButtonsPreferenceController.java
@@ -209,6 +209,15 @@ public class AppButtonsPreferenceController extends BasePreferenceController imp
     }
 
     private class UninstallAndDisableButtonListener implements View.OnClickListener {
+        private boolean mChangeEnabledStateOfUserApp;
+
+        UninstallAndDisableButtonListener() {
+            this(false);
+        }
+
+        UninstallAndDisableButtonListener(boolean changeEnabledStateOfUserApp) {
+            mChangeEnabledStateOfUserApp = changeEnabledStateOfUserApp;
+        }
 
         @Override
         public void onClick(View v) {
@@ -239,8 +248,13 @@ public class AppButtonsPreferenceController extends BasePreferenceController imp
                             mUserId);
             if (admin != null && !uninstallBlockedBySystem) {
                 RestrictedLockUtils.sendShowAdminSupportDetailsIntent(mActivity, admin);
-            } else if ((mAppEntry.info.flags & ApplicationInfo.FLAG_SYSTEM) != 0) {
+            } else if ((mAppEntry.info.flags & ApplicationInfo.FLAG_SYSTEM) != 0 || mChangeEnabledStateOfUserApp) {
                 if (mAppEntry.info.enabled && !isDisabledUntilUsed()) {
+                    if (mChangeEnabledStateOfUserApp) {
+                        handleDialogClick(ButtonActionDialogFragment.DialogType.DISABLE);
+                        return;
+                    }
+
                     // If the system app has an update and this is the only user on the device,
                     // then offer to downgrade the app, otherwise only offer to disable the
                     // app for this user.
@@ -502,6 +516,25 @@ public class AppButtonsPreferenceController extends BasePreferenceController imp
         }
 
         mButtonsPref.setButton2Enabled(enabled);
+
+        if (enabled && !isBundled) {
+            // "enabled" means "show uninstall button" in this context
+            int text;
+            int icon;
+            if (mAppEntry.info.enabled) {
+                text = R.string.disable_text;
+                icon = R.drawable.ic_settings_disable;
+            } else {
+                text = R.string.enable_text;
+                icon = R.drawable.ic_settings_enable;
+            }
+            mButtonsPref
+                    .setButton4Text(text)
+                    .setButton4Icon(icon)
+                    .setButton4Visible(true)
+                    .setButton4OnClickListener(new UninstallAndDisableButtonListener(true))
+            ;
+        }
     }
 
     /**
@@ -528,7 +561,9 @@ public class AppButtonsPreferenceController extends BasePreferenceController imp
 
     @VisibleForTesting
     void updateForceStopButton() {
-        if (mDpm.packageHasActiveAdmins(mPackageInfo.packageName)) {
+        if (!mPackageInfo.applicationInfo.enabled) {
+            mButtonsPref.setButton3Visible(false);
+        } else if (mDpm.packageHasActiveAdmins(mPackageInfo.packageName)) {
             // User can't force stop device admin.
             Log.w(TAG, "User can't force stop device admin");
             updateForceStopButtonInner(false /* enabled */);
@@ -552,6 +587,7 @@ public class AppButtonsPreferenceController extends BasePreferenceController imp
 
     @VisibleForTesting
     void updateForceStopButtonInner(boolean enabled) {
+        mButtonsPref.setButton3Visible(true);
         if (mAppsControlDisallowedBySystem) {
             mButtonsPref.setButton3Enabled(false);
         } else {
@@ -567,6 +603,7 @@ public class AppButtonsPreferenceController extends BasePreferenceController imp
             Uri packageUri = Uri.parse("package:" + packageName);
             Intent uninstallIntent = new Intent(Intent.ACTION_UNINSTALL_PACKAGE, packageUri);
             uninstallIntent.putExtra(Intent.EXTRA_UNINSTALL_ALL_USERS, allUsers);
+           //uninstallIntent.putExtra(Intent.EXTRA_UNINSTALL_SHOW_MORE_OPTIONS_BUTTON, false);
 
             mMetricsFeatureProvider.action(
                     mActivity, SettingsEnums.ACTION_SETTINGS_UNINSTALL_APP);
